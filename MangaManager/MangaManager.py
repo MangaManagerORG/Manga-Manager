93926ee8bd0815426ab9f3aaa0665e3867f58c97
RealStickman <mrc@frm01.net>
2022-02-13 15:47:49 +0100

    Fix selection index out of range error for selection = 3

--- MangaManager/MangaManager.py
+++ MangaManager/MangaManager.py
@@ -13,19 +13,22 @@ def main():
     while not selected_tool:
         selection = input("Select Number >")
         try:
-            selection = int(selection)-1
+            # make sure selection is an integer
+            selection = int(selection)
+            # create selection index for "tools" array
+            selection_index = int(selection)-1
         except:
             print("Wrong input. Select the number of the tool")
         selected_tool = True
     print(selection)
 
-    root = tk.Tk()
     if selection == 3:
         print("Not implemented yet")
-
-    app = tools[selection](root)
-    app.start_ui()
-    app.run()
+    else:
+        root = tk.Tk()
+        app = tools[selection_index](root)
+        app.start_ui()
+        app.run()
 
 if __name__ == "__main__":
     while True:
