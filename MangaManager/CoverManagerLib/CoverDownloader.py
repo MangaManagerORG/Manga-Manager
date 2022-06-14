04749a00dab8822add2285164823a3a9644d9da7
ThePromidius <thepromidiusyt@gmail.com>
2022-06-14 03:21:54 +0200

    First implementation/test

---
+++ MangaManager/CoverManagerLib/CoverDownloader.py
@@ -0,0 +1,38 @@
+import os
+import urllib.request
+from pprint import pprint
+
+import requests
+
+url = "https://api.mangadex.org"
+
+manga_id = "28c77530-dfa1-4b05-8ec3-998960ba24d4"
+if __name__ == '__main__':
+    data = {"manga[]": [manga_id], "includes[]": ["manga"]}
+    r = requests.get(url + "/cover", params=data)
+
+    r_json = r.json()
+    pprint(r_json)
+
+    print("\nfirst cover:\n")
+
+    for i, cover_data in enumerate(r_json.get("data")):
+        data = {"includes[]": ["cover_art"],
+                "order": {
+                    "createdAt": "asc",
+                    "updatedAt": "asc",
+                    "volume": "asc"
+                }
+                }
+        cover_id = cover_data.get('id')
+        cover_titles = list(filter(lambda p: p.get("type") == "manga", cover_data.get("relationships")))[0]. \
+            get("attributes").get("title")
+
+        normalized_manga_name = (cover_titles.get("jp") or cover_titles.get("en"))
+        cover_filename = cover_data.get("attributes").get("fileName")
+        cover_volume = cover_data.get("attributes").get("volume")
+        cover_loc = cover_data.get("attributes").get("locale")
+        filename, file_extension = os.path.splitext(cover_filename)
+        image_url = f"https://mangadex.org/covers/{manga_id}/{cover_filename}"
+        print(image_url)
+        urllib.request.urlretrieve(image_url, f"Cover_Vol.{str(cover_volume).zfill(2)}_{cover_loc}{file_extension}")
