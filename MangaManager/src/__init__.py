5566752692c5679661979540b04af057ead46282
ThePromidius <thepromidiusyt@gmail.com>
2022-12-19 00:17:53 +0100

    Updated env var in workflow

--- MangaManager/src/__init__.py
+++ MangaManager/src/__init__.py
@@ -5,20 +5,20 @@ from pathlib import Path
 logger = logging.getLogger()
 _CONFIG_PATH = "config.ini"
 MM_PATH = Path(Path.home(), "MangaManager")
+MM_PATH.mkdir(exist_ok=True,parents=True)
+
 DEV_BUILD = f'{environ.get("$$_ENV_DEVELOPMENT_MM_$$")}'
 DEV_BUILD = DEV_BUILD.lower() == "true"
 if DEV_BUILD:
     EXTENSIONS_DIR = Path(Path(__file__).parent.parent,"Extensions")
 else:
     EXTENSIONS_DIR = Path(MM_PATH,"Extensions")
-
 EXTENSIONS_DIR.mkdir(exist_ok=True)
 
 
 from src.settings import Settings
 
 
-MM_PATH.mkdir(exist_ok=True,parents=True)
 CONFIG_PATH = Path(MM_PATH, _CONFIG_PATH)
 
 settings_class = Settings(CONFIG_PATH)
