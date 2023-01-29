e7ad5af49d950d75518139ea5f5429c534647462
ThePromidius <thepromidiusyt@gmail.com>
2023-01-30 00:10:11 +0100

    Fixed build not running.

--- MangaManager/src/__init__.py
+++ MangaManager/src/__init__.py
@@ -17,7 +17,7 @@ logger.error(f"sub_mm_path:{sub_mm_path}")
 EXTENSIONS_DIR = Path(sub_mm_path, "Extensions")
 EXTENSIONS_DIR.mkdir(exist_ok=True)
 
-SOURCES_DIR = Path(sub_mm_path, "Sources")
+SOURCES_DIR = Path(sub_mm_path, "ExternalSources")
 SOURCES_DIR.mkdir(exist_ok=True)
 
 from src.settings import Settings
