67205d312d14c18404fada2d904d13c0461d89e1
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2023-02-04 05:44:17 -0600

    Updated the gitignore to properly cover python and JetBrains IDEs.

---
+++ MangaManager/src/MetadataManager/GUI/ControlManager.py
@@ -0,0 +1,40 @@
+import logging
+import tkinter
+
+import _tkinter
+
+logger = logging.getLogger()
+
+
+class ControlManager:
+    """
+    """
+    control_button_set = set()
+    control_hooks = []  # Callables to call when it should lock or unlock
+
+    def add(self, widget: tkinter.Widget):
+        self.control_button_set.add(widget)
+
+    def append(self, widget: tkinter.Widget):
+        self.control_button_set.add(widget)
+
+    def toggle(self, enabled=True):
+        for widget in self.control_button_set:
+            try:
+                widget.configure(state="normal" if enabled else "disabled")
+            except _tkinter.TclError:
+                logger.exception("Unhandled exception")
+
+    def lock(self):
+        self.toggle(False)
+
+    def unlock(self):
+        self.toggle(True)
+
+
+
+
+
+
+
+
