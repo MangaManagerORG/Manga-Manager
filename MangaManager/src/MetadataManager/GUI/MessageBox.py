f706ec4ff1be4da5f27b37c8a54272818f7ff026
ThePromidius <thepromidiusyt@gmail.com>
2023-02-08 22:20:46 +0100

    Refactored to use factory pattern

--- MangaManager/src/MetadataManager/GUI/MessageBox.py
+++ MangaManager/src/MetadataManager/GUI/MessageBox.py
@@ -1,31 +1,32 @@
-import enum
 from tkinter import Button, Label, Frame, Tk, Toplevel
 
 from src.MetadataManager.GUI.scrolledframe import ScrolledFrame
 from src.MetadataManager.GUI.utils import center
 
 
-class MessageBoxReturns(enum.Enum):
-    OK = 1
-    YES = 1
-    NO = 2
-    CANCEL = 3
+class MessageBoxButton:
+    def __init__(self, id, title):
+        self.id = id
+        self.title = title
 
 
-class MessageBox(Toplevel):
-    _icon_warning = "::tk::icons::warning"
-    _icon_error = "::tk::icons::error"
-    _icon_information = "::tk::icons::information"
-    _icon_question = "::tk::icons::question"
+class MessageBoxWidget(Toplevel):
+    """A Toplevel widget that represents a message box with a title, content, and buttons."""
+
+    icon_warning = "::tk::icons::warning"
+    icon_error = "::tk::icons::error"
+    icon_information = "::tk::icons::information"
+    icon_question = "::tk::icons::question"
 
     selected_value = None
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         # Setting Geometry
         self.geometry("300x120+100+100")
-        
-        self.label = Label(self, text="Dummy_Text",font=("Helvetica",18))
+
+        self.label = Label(self, text="Dummy_Text", font=("Helvetica", 18))
         self.label.pack(side="top")
 
         frame = ScrolledFrame(master=self, scrolltype="vertical", usemousewheel=True)
@@ -42,41 +43,59 @@ class MessageBox(Toplevel):
         # Making MessageBox Visible
         center(self)
 
-    def info(self, title, text):
-        self.title = title
-        self.label.configure(image=self._icon_information, compound="left")
-        self.label.configure(text=text)
-        # self.wait_window()
+    def with_icon(self, icon_path):
+        """Adds an icon to the MessageBoxWidget.
 
-    def warning(self, title, text):
-        self.title = title
-        self.label.configure(image=self._icon_warning, compound="left")
-        self.label.configure(text=text)
-        # self.wait_window()
-
-    def error(self, title, text):
-        self.title = title
-        self.label.configure(image=self._icon_error, compound="left")
-        self.label.configure(text=text)
-        # self.wait_window()
-
-
-    def yes_no_question(self, title, text):
+        :param icon_path: The path to the icon image.
+        :return: The MessageBoxWidget instance.
         """
-        Asks a question. Buttons displayed are [Yes, No]
-        :param title:
-        :param text:
-        :return:
-        """
-        # Update title and main label
-        self.title = title
-        self.label.configure(image=self._icon_question, compound="left")
-        self.label.configure(text=text)
+        self.label.configure(image=icon_path, compound="left")
+        return self
 
-        # Setup buttons
-        Button(self.control_frame,text="Yes",padx=10,pady=5, borderwidth=3, command=lambda:self.set_selected_value(MessageBoxReturns.YES)).pack(side="left", ipadx=20)
-        Label(self.control_frame).pack(side="left",padx=5)
-        Button(self.control_frame, text="No",padx=10,pady=5, borderwidth=3, command=lambda:self.set_selected_value(MessageBoxReturns.NO)).pack(side="left", ipadx=20)
+    def with_title(self, title):
+        """
+        Adds a title to the MessageBoxWidget.
+        :param title: The text for the title.
+        :return: The MessageBoxWidget instance.
+        """
+        self.title = title
+        self.label.configure(text=title)
+        return self
+
+    def with_content(self, content_frame):
+        """
+        Adds content to the MessageBoxWidget.
+        :param content_frame: The content frame.
+        :return: The MessageBoxWidget instance.
+        """
+        # I didn't see how you did this, but we can have with_content_frame or with_content
+        return self
+
+    def with_actions(self, action_buttons):
+        """
+        Adds buttons to the MessageBoxWidget.
+        :param action_buttons: A list of MessageBoxButton instances.
+        :return: The MessageBoxWidget instance.
+        """
+        # button is a MessageBoxButton class with id, title
+        for button in action_buttons:
+            Button(self.control_frame, text=button.title, padx=10, pady=5, borderwidth=3,
+                   command=lambda: self._set_selected_value(button.id)).pack(side="left", ipadx=20)
+            Label(self.control_frame).pack(side="left", padx=5)
+        return self
+
+    def build(self):
+        """
+        Builds the MessageBoxWidget.
+        :return: The MessageBoxWidget instance.
+        """
+        return self
+
+    def prompt(self):
+        """
+        Displays the MessageBoxWidget and waits for a button press.
+        :return: The selected value (the value of the pressed button).
+        """
 
         # Set to wait for window to get deleted.
         self.wm_protocol("WM_DELETE_WINDOW", self.destroy)
@@ -84,17 +103,19 @@ class MessageBox(Toplevel):
         # return the value
         return self.selected_value
 
-    def set_selected_value(self, value):
+    def _set_selected_value(self, value):
+        """
+        Sets the selected value of the MessageBoxWidget.
+        :param value: The selected value.
+        """
         self.selected_value = value
         self.destroy()
 
 
 if __name__ == '__main__':
     root = Tk()
-    a = MessageBox(root)
-    print(a.info("Info Message", "Info Message"))
-    print(a.warning("Warn Message", "Warning Message"))
-    print(a.error("Error Message", "Error Message"))
-    print(a.yes_no_question("Question Message", "Question Message"))
-    # a.get_selected_value()
+    a = MessageBoxWidget(root).with_title("Test Message").with_actions(
+        [MessageBoxButton(0, "No"), MessageBoxButton(1, "Yes")]).with_icon(MessageBoxWidget.icon_question)
+    print(a.prompt())
+
     root.mainloop()
